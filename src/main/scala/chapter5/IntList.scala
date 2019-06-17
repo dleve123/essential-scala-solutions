5e9bd6abb2275cb643690bcd74f7493c68578a11
Daniel Levenson <dleve123@gmail.com>
2019-06-17 12:21:36 -0400

    Reorganize InList methods

--- src/main/scala/chapter5/IntList.scala
+++ src/main/scala/chapter5/IntList.scala
@@ -1,7 +1,6 @@
 package chapter5
 
 sealed trait IntList {
-
   def fold(end: Int, f: (Int, Int) => Int): Int =
     this match {
       case IntEnd => end
@@ -9,16 +8,14 @@ sealed trait IntList {
     }
 
   def length: Int = fold(0, (head, tail) => 1 + tail)
+  def product: Int = fold(1, (head, tail) => head * tail)
+  def sum: Int = fold(0, (head, tail) => head + tail)
 
   def double: IntList =
     this match {
       case IntEnd => IntEnd
       case IntPair(hd, tl) => IntPair(hd * 2, tl.double)
     }
-
-  def product: Int = fold(1, (head, tail) => head * tail)
-
-  def sum: Int = fold(0, (head, tail) => head + tail)
 }
 
 case object IntEnd extends IntList
