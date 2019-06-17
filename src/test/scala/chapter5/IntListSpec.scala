1bf54a590739beee1a603cda7ed9530eecb0491f
Daniel Levenson <dleve123@gmail.com>
2019-06-17 12:09:01 -0400

    Add tests for IntList#length

--- src/test/scala/chapter5/IntListSpec.scala
+++ src/test/scala/chapter5/IntListSpec.scala
@@ -16,4 +16,18 @@ class IntListSpec extends UnitSpec {
       assert(example.sum == 0)
     }
   }
+
+  describe("#length") {
+    it("returns 0 when there is an empty IntList") {
+      val example = IntEnd
+
+      assert(example.length == 0)
+    }
+
+    it("returns the count of the number of non-End elements") {
+      val example = IntPair(1, IntPair(2, IntPair(3, IntEnd)))
+
+      assert(example.length == 3)
+    }
+  }
 }
